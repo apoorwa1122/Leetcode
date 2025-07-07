a485169cd48a8d9b67693e1b8c772c0b87c0024d
APOORVA SINGH <146919913+apoorwa1122@users.noreply.github.com>
2025-07-07 09:46:58 +0530

    Time: 103 ms (96.88%), Space: 54.6 MB (5.16%) - LeetHub

---
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxEvents(self, events: List[List[int]]) -> int:
+        INF = 10 ** 20
+        evt = collections.defaultdict(list)
+
+        for s, e in events:
+            evt[s].append(e)
+        
+        count = 0
+        lt = INF
+        h = []
+        for t in sorted(evt.keys()):
+            while len(h) > 0 and lt < t:
+                e = heapq.heappop(h)
+                if e >= lt:
+                    count += 1
+                    lt += 1
+                else:
+                    pass
+            lt = t
+
+            for e in evt[t]:
+                heapq.heappush(h, e)
+        t = INF
+
+        while len(h) > 0 and lt < t:
+            e = heapq.heappop(h)
+            if e >= lt:
+                count += 1
+                lt += 1
+        return count
